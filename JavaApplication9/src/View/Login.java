983d40828b3c51494d98fe3e2d55b8066cc7283a
Lichking1830 <109405414+Lichking1830@users.noreply.github.com>
2022-12-06 23:23:26 +0700

    up lại khung lần 3

--- JavaApplication9/src/View/Login.java
+++ JavaApplication9/src/View/Login.java
@@ -1,20 +1,338 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package View;
+package Views;
 
-/**
- *
- * @author ADMIN
- */
-public class Login {
+import DomainModel.TaiKhoan;
+import Service.Impl.TaiKhoanServiceImpl;
+import java.awt.Cursor;
+import java.awt.Font;
+import java.awt.font.TextAttribute;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Login extends javax.swing.JFrame {
+
+    private List<String> acc = new ArrayList<>();
+    private TaiKhoan tk = new TaiKhoan();
+    private TaiKhoanServiceImpl tksi = new TaiKhoanServiceImpl();
+
+    public Login() {
+        initComponents();
+        pfPass.setEchoChar('\u25cf');
     }
-    
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPasswordField1 = new javax.swing.JPasswordField();
+        kGradientPanel1 = new keeptoo.KGradientPanel();
+        jLabel6 = new javax.swing.JLabel();
+        txtUserName = new javax.swing.JTextField();
+        pfPass = new javax.swing.JPasswordField();
+        btLogin = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        lbDefault = new javax.swing.JLabel();
+        lbRegister = new javax.swing.JLabel();
+        cbShow = new javax.swing.JCheckBox();
+        lbExit = new javax.swing.JLabel();
+
+        jPasswordField1.setText("jPasswordField1");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        jLabel6.setFont(new java.awt.Font(".VnArabiaH", 1, 48)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Login");
+
+        txtUserName.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        txtUserName.setForeground(new java.awt.Color(255, 255, 255));
+        txtUserName.setText("UserName");
+        txtUserName.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 3, 0, new java.awt.Color(255, 255, 255)));
+        txtUserName.setOpaque(false);
+        txtUserName.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                txtUserNameMouseClicked(evt);
+            }
+        });
+
+        pfPass.setFont(new java.awt.Font("Segoe UI Black", 0, 18)); // NOI18N
+        pfPass.setForeground(new java.awt.Color(255, 255, 255));
+        pfPass.setText("password");
+        pfPass.setBorder(javax.swing.BorderFactory.createMatteBorder(0, 0, 3, 0, new java.awt.Color(255, 255, 255)));
+        pfPass.setOpaque(false);
+
+        btLogin.setBackground(new java.awt.Color(255, 0, 51));
+        btLogin.setFont(new java.awt.Font(".VnBahamasBH", 0, 18)); // NOI18N
+        btLogin.setForeground(new java.awt.Color(255, 255, 255));
+        btLogin.setText("SIGN IN");
+        btLogin.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btLogin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btLoginActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/male_user_40px.png"))); // NOI18N
+        jLabel1.setText("jLabel1");
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/password_40px.png"))); // NOI18N
+        jLabel2.setText("jLabel2");
+
+        lbDefault.setFont(new java.awt.Font("Microsoft New Tai Lue", 0, 12)); // NOI18N
+        lbDefault.setForeground(new java.awt.Color(255, 255, 255));
+        lbDefault.setText("Don't have an account?");
+
+        lbRegister.setForeground(new java.awt.Color(102, 255, 0));
+        lbRegister.setText("Sign Up");
+        lbRegister.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lbRegister.setDebugGraphicsOptions(javax.swing.DebugGraphics.NONE_OPTION);
+        lbRegister.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lbRegisterMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                lbRegisterMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                lbRegisterMouseExited(evt);
+            }
+        });
+
+        cbShow.setForeground(new java.awt.Color(255, 255, 255));
+        cbShow.setText("Show Password");
+        cbShow.setOpaque(false);
+        cbShow.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbShowActionPerformed(evt);
+            }
+        });
+
+        lbExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Image/Close Pane_40px.png"))); // NOI18N
+        lbExit.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        lbExit.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lbExitMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout kGradientPanel1Layout = new javax.swing.GroupLayout(kGradientPanel1);
+        kGradientPanel1.setLayout(kGradientPanel1Layout);
+        kGradientPanel1Layout.setHorizontalGroup(
+            kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(kGradientPanel1Layout.createSequentialGroup()
+                .addGroup(kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(kGradientPanel1Layout.createSequentialGroup()
+                        .addGap(37, 37, 37)
+                        .addGroup(kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(btLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 309, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(kGradientPanel1Layout.createSequentialGroup()
+                                .addGroup(kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addGroup(kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(cbShow)
+                                    .addGroup(kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(pfPass)
+                                        .addComponent(txtUserName, javax.swing.GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE))))))
+                    .addGroup(kGradientPanel1Layout.createSequentialGroup()
+                        .addGap(108, 108, 108)
+                        .addComponent(lbDefault)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lbRegister)))
+                .addContainerGap(45, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, kGradientPanel1Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, kGradientPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel6)
+                        .addGap(119, 119, 119))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, kGradientPanel1Layout.createSequentialGroup()
+                        .addComponent(lbExit, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())))
+        );
+        kGradientPanel1Layout.setVerticalGroup(
+            kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(kGradientPanel1Layout.createSequentialGroup()
+                .addGap(4, 4, 4)
+                .addComponent(lbExit)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel6)
+                .addGap(63, 63, 63)
+                .addGroup(kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtUserName, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(31, 31, 31)
+                .addGroup(kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(pfPass, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(cbShow)
+                .addGap(35, 35, 35)
+                .addComponent(btLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(kGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbDefault)
+                    .addComponent(lbRegister))
+                .addContainerGap(91, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(kGradientPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(kGradientPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btLoginActionPerformed
+        String user = txtUserName.getText();
+        String pass = pfPass.getText();
+        TaiKhoan check = tksi.Check(user, pass);
+        if (check != null) {
+            Home h = new Home();
+            HomeUser hu = new HomeUser();
+            acc.add(user);
+            acc.add(pass);
+            String path = System.getProperty("user.dir");
+            ghihFile(path, acc);
+            String vaiTro = check.getVaiTro();
+            if (vaiTro.equalsIgnoreCase("QUANTRIVIEN")) {
+                JOptionPane.showMessageDialog(rootPane, "Bạn đăng vào hệ thống Quản Lý");
+                h.setVisible(true);
+                this.dispose();
+            }
+            if (vaiTro.equalsIgnoreCase("NHANVIEN")) {
+                JOptionPane.showMessageDialog(rootPane, "Bạn đăng vào hệ thống Nhân Viên");
+                hu.setVisible(true);
+                this.dispose();;
+            }
+        } else {
+            JOptionPane.showMessageDialog(rootPane, "Bạn chưa có tài khoản");
+        }
+    }//GEN-LAST:event_btLoginActionPerformed
+
+    private void cbShowActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbShowActionPerformed
+        if (cbShow.isSelected()) {
+            pfPass.setEchoChar((char) 0);
+        } else {
+            pfPass.setEchoChar('\u25cf');
+        }
+    }//GEN-LAST:event_cbShowActionPerformed
+
+    private void lbExitMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbExitMouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_lbExitMouseClicked
+
+    private void txtUserNameMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_txtUserNameMouseClicked
+        if (txtUserName.getText().equals("UserName")) {
+            txtUserName.setText("");
+        }
+    }//GEN-LAST:event_txtUserNameMouseClicked
+
+    private void lbRegisterMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbRegisterMouseClicked
+        Register reg = new Register();
+        reg.setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_lbRegisterMouseClicked
+
+    private void lbRegisterMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbRegisterMouseEntered
+        Font font = lbRegister.getFont();
+        Map attributes = font.getAttributes();
+        attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+        lbRegister.setFont(font.deriveFont(attributes));
+    }//GEN-LAST:event_lbRegisterMouseEntered
+
+    private void lbRegisterMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lbRegisterMouseExited
+        Font font = lbRegister.getFont();
+        Map attributes = font.getAttributes();
+        attributes.put(TextAttribute.UNDERLINE, -1);
+        lbRegister.setFont(font.deriveFont(attributes));
+    }//GEN-LAST:event_lbRegisterMouseExited
+
+    public String ghihFile(String path, List<String> lists) {
+        String result = "";
+        try {
+            File file = new File("docghi.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            for (String tk : lists) {
+                oos.writeObject(tk);
+            }
+            oos.close();
+            fos.close();
+            result = "Ghi File thanh cong";
+        } catch (Exception e) {
+            result = "Ghi File that bai";
+        }
+        return result;
+    }
+
+//    public static void main(String args[]) {
+//        /* Set the Nimbus look and feel */
+//        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+//        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+//         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+//         */
+//        try {
+//            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+//                if ("Nimbus".equals(info.getName())) {
+//                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+//                    break;
+//                }
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (InstantiationException ex) {
+//            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+//            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+//        }
+//        //</editor-fold>
+//        //</editor-fold>
+//        //</editor-fold>
+//        //</editor-fold>
+//
+//        /* Create and display the form */
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new Login().setVisible(true);
+//            }
+//        });
+//    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btLogin;
+    private javax.swing.JCheckBox cbShow;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPasswordField jPasswordField1;
+    private keeptoo.KGradientPanel kGradientPanel1;
+    private javax.swing.JLabel lbDefault;
+    private javax.swing.JLabel lbExit;
+    private javax.swing.JLabel lbRegister;
+    private javax.swing.JPasswordField pfPass;
+    private javax.swing.JTextField txtUserName;
+    // End of variables declaration//GEN-END:variables
 }
