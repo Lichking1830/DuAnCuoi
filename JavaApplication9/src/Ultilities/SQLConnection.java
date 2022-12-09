4b4a13bb08119e5e7d0987462f739fee0d1bc7b0
THANH2412\HP <thanhkon2412@gmail.com>
2022-12-09 15:24:35 +0700

    sql

--- JavaApplication9/src/Ultilities/SQLConnection.java
+++ JavaApplication9/src/Ultilities/SQLConnection.java
@@ -16,7 +16,7 @@ public class SQLConnection {
 
     public static final String HOSTNAME = "localhost";
     public static final String PORT = "1433";
-    public static final String DBNAME = "QUANLYCUAHANG";
+    public static final String DBNAME = "QLCH";
     public static final String USERNAME = "sa";
     public static final String PASSWORD = "123123";
 
